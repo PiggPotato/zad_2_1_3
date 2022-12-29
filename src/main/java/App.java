760451c2abc46be0178ecb98feb4fc28d1c94186
PiggPotato <aldenmen12@gmail.com>
2022-12-29 18:53:32 +0300

    переделал

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,9 +9,16 @@ public class App {
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
 
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
         Cat cat = applicationContext.getBean("cat", Cat.class);
         Cat cat2 = applicationContext.getBean("cat", Cat.class);
         System.out.println(cat);
         System.out.println(cat2);
+
+        System.out.println("Сравнение Hello world, " + (bean == bean2));
+        System.out.println("Сравнение Cat, " + (cat == cat2));
     }
 }
